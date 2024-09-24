d0a7595642a133e9d47f742e82072782dc8fe95f
emma2625 <emmanuelodobo10@gmail.com>
2024-09-24 12:34:30 +0100

    Updated Changes Made

--- resources/views/pages/welcome.blade.php
+++ resources/views/pages/welcome.blade.php
@@ -8,10 +8,10 @@
       <!-- Meet Up starts -->
       <section class="container mb-5 mt-5 color">
         <div class="row  mx-auto ">
-            <div class="col-6">
+            <div class="col-md-6 mb-5">
                 <img src="{{ asset("images/img-1.jpg") }}" class="order-1 order-md-0 w-100" alt=""> 
             </div>
-            <div class="col-6 order-1 order-md-0 text-center meet-div " >
+            <div class="col-md-6 order-1 order-md-0 text-center meet-div " >
                 <h2 class="w-50 pri">Spotlight MEETUP.</h2>
                 <div class="text-start  ">
                     <p class="Meet_p">Meet Kindred Book Lovers In A Local Meetup Book Club!.
@@ -45,40 +45,34 @@
     {{-- New Arrivals --}}
     <div class="container mb-5">
         <h2 class="mt-4">New Arrivals</h2>
-        <div class="row">
-            <div class="col-md-4">
-                <div class="card arrival_card">
-                    <img src="{{ asset("images/img-1.jpg") }}" class="card-img-top arrival_img" alt="New Book 1">
-                    <div class="card-body position-relative">
-                        <h5 class="card-title">New Book Title 1</h5>
-                        <p  class="card-text text-truncate" style="max-width: 320px;">Latest insights into the world of technology and innovation.</p>
-                        <a href="#" class="btn btn-warning  position-absolute bottom-0 mb-1 text-white rounded-0">Read More</a>
-                    </div>
+        <div class="library_container container mt-5">
+            <div class="books">
+                <div class="row">
+                    @for ($i = 0; $i < 4; $i++)
+                        <div class="col-md-4 col-lg-3 mb-5">
+                            <a href="" class="card border nav-link h-100 rounded-0 book">
+                                <div class="box">
+                                    <img src="{{ asset('images/img-1.jpg') }}" class="" alt="cover">
+                                </div>
+                                <div class="mt-2 card-body">
+                                    <div class="row mb-2">
+                                        <span class="col-1 fa-solid fa-book text-warning"></span>
+                                        <span class="col-10">
+                                            {{ fake()->realText(50) }}
+                                        </span>
+                                    </div>
+                                    <div class="row mb-2">
+                                        <span class="col-1 fa-solid fa-user-edit text-warning"></span>
+                                        <span class="col-10">
+                                            {{ fake()->name }}
+                                        </span>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+                    @endfor
                 </div>
             </div>
-            <div class="col-md-4">
-                <div class="card arrival_card">
-                    <img src="{{ asset("images/img-2.jpg") }}" class="card-img-top arrival_img" alt="New Book 2">
-                    <div class="card-body position-relative">
-                        <h5 class="card-title">New Book Title 2</h5>
-                        <p class="card-text text-truncate" style="max-width: 320px;">Explore the depths of the ocean and its mysteries.</p>
-                        <a href="#" class="btn btn-warning position-absolute bottom-0  mb-1 text-white  rounded-0">Read More</a>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-4">
-                <div class="card arrival_card">
-                    <img src="{{ asset('images/img-3.jpg') }}" class="card-img-top arrival_img" alt="New Book 3">
-                    <div class="card-body">
-                        <h5 class="card-title">New Book Title 3</h5>
-                        <p class="card-text text-truncate" style="max-width: 320px;">
-                            A riveting tale of adventure and survival that takes you on an unforgettable journey through uncharted territories.
-                        </p>
-                        <a href="#" class="btn btn-warning position-absolute bottom-0 mb-1 text-white  rounded-0">Read More</a>
-                    </div>
-                </div>
-            </div>
-            
         </div>
     </div>
     {{-- New Arrivals Ends --}}
